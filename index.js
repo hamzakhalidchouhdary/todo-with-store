b1fa5043f1592f4b57e93d0ce64824d5fdd82c55
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-06-02 22:34:33 +0500

    added method to create list item element

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+const todoContainer = document.querySelector('#todo-container');
+
+function createTodoElement(text) {
+  if (!text) return;
+  const todoItemWrapper = document.createElement('li');
+  const todoRemoveBtn = document.createElement('button');
+  const todoTextEle = document.createElement('p');
+  todoRemoveBtn.innerText = 'X';
+  todoRemoveBtn.classList.add('btn','btn-outline-danger', 'btn-md', 'col-1');
+  todoItemWrapper.classList.add('row', 'py-3', 'border-top', 'border-bottom', 'border-light-subtle');
+  todoTextEle.classList.add('col-11', 'm-0');
+  todoTextEle.innerText = text;
+  todoItemWrapper.appendChild(todoTextEle);
+  todoItemWrapper.appendChild(todoRemoveBtn);
+  return todoItemWrapper
+}
+
 function addNewTodoItem(event) {
   const text = event.target.value;
   event.target.value = null;
-  console.log(text);
+  const newItem = createTodoElement(text);
+  todoContainer.append(newItem);
 }
