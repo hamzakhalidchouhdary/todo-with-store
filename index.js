4aa2ddc1e3cfd5362fdb5b8265270115d2b45fc8
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-06-09 18:17:28 +0500

    update formatting

--- index.js
+++ index.js
@@ -1,51 +1,57 @@
-const todoContainer = document.querySelector('#todo-container');
+const todoContainer = document.querySelector("#todo-container");
 
 var store = [];
 
-function createTodoElement({text, id}) {
+function createTodoElement({ text, id }) {
   if (!text) return;
-  const todoItemWrapper = document.createElement('li');
-  const todoRemoveBtn = document.createElement('button');
-  const todoTextEle = document.createElement('p');
-  todoRemoveBtn.innerText = 'X';
-  todoRemoveBtn.addEventListener('click', () => removeItem(id));
-  todoRemoveBtn.classList.add('btn','btn-outline-danger', 'btn-md', 'col-1');
-  todoItemWrapper.classList.add('row', 'py-3', 'border-top', 'border-bottom', 'border-light-subtle');
-  todoTextEle.classList.add('col-11', 'm-0');
+  const todoItemWrapper = document.createElement("li");
+  const todoRemoveBtn = document.createElement("button");
+  const todoTextEle = document.createElement("p");
+  todoRemoveBtn.innerText = "X";
+  todoRemoveBtn.addEventListener("click", () => removeItem(id));
+  todoRemoveBtn.classList.add("btn", "btn-outline-danger", "btn-md", "col-1");
+  todoItemWrapper.classList.add(
+    "row",
+    "py-3",
+    "border-top",
+    "border-bottom",
+    "border-light-subtle"
+  );
+  todoTextEle.classList.add("col-11", "m-0");
   todoTextEle.innerText = text;
   todoItemWrapper.appendChild(todoTextEle);
   todoItemWrapper.appendChild(todoRemoveBtn);
-  return todoItemWrapper
+  return todoItemWrapper;
 }
 
 function renderItems() {
-  todoContainer.innerHTML = '';
-  store.forEach(item => {
+  todoContainer.innerHTML = "";
+  store.forEach((item) => {
     const newItem = createTodoElement(item);
     todoContainer.append(newItem);
   });
-};
+}
 
 function addNewItem(text) {
   store.push({
     id: Date.now(),
-    text
+    text,
   });
   storeState();
 }
 
 function storeState() {
-  localStorage.setItem('todoWithStore', JSON.stringify(store));
+  localStorage.setItem("todoWithStore", JSON.stringify(store));
   renderItems();
 }
 
 function restoreState() {
-  store = JSON.parse(localStorage.getItem('todoWithStore')) || [];
+  store = JSON.parse(localStorage.getItem("todoWithStore")) || [];
   renderItems();
 }
 
-function removeItem (itemId) {
-  store = store.filter(({id}) => id !== itemId );
+function removeItem(itemId) {
+  store = store.filter(({ id }) => id !== itemId);
   storeState();
 }
 
